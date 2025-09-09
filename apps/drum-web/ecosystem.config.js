f48792301d1fb19d094074bea62713e5506c6384
alivanz <alivan1627@gmail.com>
2025-09-10 02:34:26 +0800

    web ecosystem

---
+++ apps/drum-web/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "drum-web",
+      script: "pnpm",
+      args: "dev",
+      interpreter: "none", // tells PM2 not to use node, just run the command directly
+    },
+  ],
+};
