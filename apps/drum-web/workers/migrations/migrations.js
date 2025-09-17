1227d52e2929534a08d93d7e9d38c2d97122989c
alivanz <alivan1627@gmail.com>
2025-09-17 21:14:58 +0700

    fix migrate

--- apps/drum-web/workers/migrations/migrations.js
+++ apps/drum-web/workers/migrations/migrations.js
@@ -1,6 +1,6 @@
 import journal from './meta/_journal.json';
-import m0000 from './0000_married_slipstream.sql';
-import m0001 from './0001_condemned_umar.sql';
+import m0000 from './0000_married_slipstream.sql?raw';
+import m0001 from './0001_condemned_umar.sql?raw';
 
   export default {
     journal,
