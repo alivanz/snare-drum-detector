04165f046a402800976918426dfcc4d230709a96
alivanz <alivan1627@gmail.com>
2025-09-17 23:20:40 +0700

    try fix micromamba

--- start-detector.bat
+++ start-detector.bat
@@ -1,5 +1,10 @@
 @echo off
 echo Starting Snare Drum Detector...
 cd /d "%~dp0apps\detector"
-micromamba run -n drum python main.py -w
+
+REM Initialize micromamba for this session
+call micromamba shell hook -s cmd.exe
+call micromamba activate drum
+
+python main.py -w
 pause
