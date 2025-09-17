f1b0c1f72f23c7f9bbdf8778f9a3147c37973cfc
alivanz <alivan1627@gmail.com>
2025-09-17 23:23:06 +0700

    try

--- start-detector.bat
+++ start-detector.bat
@@ -2,9 +2,6 @@
 echo Starting Snare Drum Detector...
 cd /d "%~dp0apps\detector"
 
-REM Initialize micromamba for this session
-call micromamba shell hook -s cmd.exe
-call micromamba activate drum
-
-python main.py -w
+REM Run python with explicit environment activation
+micromamba run -n drum python -u main.py -w
 pause
